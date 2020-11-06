6409b399a0f8e7574910dcfb93b22ae40ea6ae6e
stephn2027 <stephn2027@gmail.com>
2020-11-06 21:54:52 +0900

    11/06/2020

--- projects/drumkit/myscript.js
+++ projects/drumkit/myscript.js
@@ -1,10 +1,18 @@
 window.addEventListener('keydown', function(keyDownFunc){
 const audio = document.querySelector(`audio[data-key="${keyDownFunc.keyCode}"]`);
 const key = document.querySelector(`.key[data-key="${keyDownFunc.keyCode}"]`);
-if(!audio) alert("Key not registered")
-	text = console.log("Key not registered"); // stops the function from running
+if(!audio) return; // stops the function from running
 
 audio.currentTime = 0;//rewinds the key to start
 audio.play();
 key.classList.add('playing');
 });
+
+function removeTransition(keyDownFunc){
+if(keyDownFunc.propertyName !== 'transform') return; //skip if not a transform
+this.classList.remove('playing');
+
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key=>key.addEventListener('transitionend',removeTransition));
